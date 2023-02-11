cb28c0070237aeed7ee01a96479ef755f05bd7ed
unknown <cheo28sc@hotmail.com>
2023-02-10 20:52:51 -0600

    Mi primer clase con phyton

---
+++ clase.py
@@ -0,0 +1,81 @@
+# last_name = "Segura"
+# name = "Esteban"
+# print (name+ " "+ last_name)
+
+
+#Diferentes maneras de expresar variables
+# name = "Esteban"
+# id = 112180982
+# money = 0.02
+# turn_on = True
+
+# print ("Load Data")
+# status = False
+# print (status)
+
+# subcribed = True
+# print (subcribed)
+
+#Ejemplo de un interruptor
+# is_day = False
+# lights_on = not is_day
+# print ("DayTime")
+# print(is_day)
+
+# print ("lights on?")
+# print (lights_on)
+
+
+#Codigo de ventas y stock
+# stock = 600
+# jeans_sold = 500
+# target = 500
+
+# target_hit = jeans_sold == target
+# print ("hit jeans sale target: ")
+# print (target_hit)
+
+# current_stock = stock - jeans_sold
+# in_stock = current_stock != 0
+# print ("jeans in stock: ")
+# print (in_stock)
+
+#comparacion de numeros y ejemplos
+
+# result = 9 >= 15
+# print (result)
+# min = 5
+# max = 10
+# result = min >= max
+# print (result)
+
+
+# battery_level = 20
+# low = battery_level <= 14
+# print ("low battery:  ")
+# print (low)
+
+#Comparaciones de cadenas de texto
+# print ("online" == "online ")
+# print ("online " != "offline ")
+
+#Medidor de frecuencia cardiaca
+# heart_rate = 150
+# too_low = heart_rate < 60
+# too_high = heart_rate > 100
+# print ("Heart rate low: ")
+# print (too_low)
+
+# print ("Heart rate High: ")
+# print (too_high)
+
+# cadena de formato
+new_messages = 5
+print (f"{new_messages} new messages")
+
+new = 5
+read = 2
+print (f"{new-read} unread messages")
+
+print (f"{5} new messages and {2} fried request: ")
+
