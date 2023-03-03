5e0a257a762cf839b7a5482b466000b21e2c10bb
Esteban <cheo28sc@hotmail.com>
2023-03-02 21:10:55 -0600

    tuplas y funciones

---
+++ clase4.py
@@ -0,0 +1,34 @@
+#declaraciones elif
+
+# def calculate(operator, x, y):
+#     if operator == "+":
+#         print (x + y)
+#     elif operator == "-":
+#         print (x - y)
+#     else:
+#         print(f"unknown:{operator} ")
+#         calculate("*", 40 , 30)
+    #valor de entrada como de salida   
+# 
+# 
+
+# def onboard_passengers(bookings):
+#     counter = 1
+#     while counter <= bookings:
+#         print(f"passenger {counter} on board")
+#         counter += 1
+#         onboard_passengers(15)  
+
+#    la utilizacion de un bucle
+# def halve_price(cart):
+#     for price in cart:
+#         print (f"new price: {price/2}")
+# cart_list = [5, 20, 8]
+# halve_price(cart_list)   
+
+# def display_players(team):
+#     number  = 1
+#     for name in team:
+#         print (f"player {number}:{name} ")
+# team1 = ["kim", "lee"]
+# team2 = ["jos", "teban"]
--- practicando.py
+++ practicando.py
@@ -2,5 +2,14 @@
 
 # for i in range(20):
 #     print(i, end=" ") #imprimir numero, sin salto linea
-# print() #lineavacia
+# print() #lin
+print ("lets see how long you have lived in days, minutes and seconds.")
+name = input ("name: ")
+print (" how enter  your age: ")
+age = int(input("age: "))
 
+days = age * 365
+minutes = age * 525948
+seconds = age * 31556926
+
+print (name , "has bee  alive for ", days "days", minutes "minutes add", seconds)"seconds! wow: "
