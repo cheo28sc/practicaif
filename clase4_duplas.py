fc60c8bf17b25ba83bff01c71e84427ae430a2b0
Esteban <cheo28sc@hotmail.com>
2023-03-02 21:13:28 -0600

    tuplas

---
+++ clase4_duplas.py
@@ -0,0 +1,32 @@
+# movies  = ["vertigo", "chuky", 1985 , 2019]
+# #las tuplas son para agrupar datos relacionados
+# movies_tupla = [("vertigo",1985), ("chuchy", 2019)]
+
+# vertigo_data = ("vertigo",1985)
+# print (movies_tupla)
+
+# user_data ("scastro",)
+# print (user_data)
+
+#    nos devuelven multiples valores de una funcion
+# def get_score_datos(score_list):
+#     highest_score = max(score_list)
+#     lowest_score = min(score_list)
+#     return highest_score, lowest_score
+# scores = [31, 23, 35]
+# data = get_score_datos(scores)
+# print (data)
+# highest = data [0]
+# smallest = data [1]
+# print (f"smallest score:"{smallest})
+# print (f"highest score: {highest}")
+#ordenar
+def get_scores_datos(score_list):
+    highest_score = max(score_list)
+    
+    return highest_score
+scores = [31, 23, 35]
+data = get_scores_datos(scores)
+print (data)
+
+print (f"highest score: {highest}")
