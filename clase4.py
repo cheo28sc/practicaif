4fb17a968c9fef691d5c95a78a9c3d8aa9deaa81
Esteban <cheo28sc@hotmail.com>
2023-03-04 19:41:18 -0600

    fin_3

--- clase4.py
+++ clase4.py
@@ -7,28 +7,42 @@
 #         print (x - y)
 #     else:
 #         print(f"unknown:{operator} ")
-#         calculate("*", 40 , 30)
-    #valor de entrada como de salida   
-# 
-# 
+#         calculate("-", 40 , 30)
+# def is_multiplayer(players):
+#     print(len(players) == 2)
+    
+# players = ["Freddy", "Esteban"]
+# is_multiplayer(players)
+    
+#funciones con bucles 
+def onbord_passengers(bookings):
+    Counter = 1
+    while Counter <= bookings:
+        print(f"Passengers {Counter} on board") 
+        Counter += 1
+        
+onbord_passengers(15) 
 
-# def onboard_passengers(bookings):
-#     counter = 1
-#     while counter <= bookings:
-#         print(f"passenger {counter} on board")
-#         counter += 1
-#         onboard_passengers(15)  
+def display_progress(total_files):
+    for i in range(total_files):
+        i += 1
+        print(f"Downloading file {i} out of {total_files}")
+        
+display_progress(3)
 
 #    la utilizacion de un bucle
-# def halve_price(cart):
-#     for price in cart:
-#         print (f"new price: {price/2}")
-# cart_list = [5, 20, 8]
-# halve_price(cart_list)   
+def halve_price(cart):
+    for price in cart:
+        print (f"new price: {price/2}")
+cart_list = [5, 20, 8]
+halve_price(cart_list)   
 
-# def display_players(team):
-#     number  = 1
-#     for name in team:
-#         print (f"player {number}:{name} ")
-# team1 = ["kim", "lee"]
-# team2 = ["jos", "teban"]
+def display_players(team):
+    number  = 1
+    for name in team:
+        print (f"player {number}:{name} ")
+        number += 1
+team1 = ["kim", "lee", "Esteban"]
+team2 = ["jos", "teban"]
+
+display_players(team2)
--- practicando.py
+++ practicando.py
@@ -1,15 +1,40 @@
-###Ejemplo for, imprime los 20 primeros numeros en una linea
+##Ejemplo for, imprime los 20 primeros numeros en una linea
 
 # for i in range(20):
 #     print(i, end=" ") #imprimir numero, sin salto linea
 # print() #lin
-print ("lets see how long you have lived in days, minutes and seconds.")
-name = input ("name: ")
-print (" how enter  your age: ")
-age = int(input("age: "))
 
-days = age * 365
-minutes = age * 525948
-seconds = age * 31556926
+# varios = ['Rojo', 7 , 10]
+# suma = varios[1] + varios[2]
 
-print (name , "has bee  alive for ", days "days", minutes "minutes add", seconds)"seconds! wow: "
+# print(f"Mi color favorito es el {varios[0]} y como resultado: {suma}.")
+    
+    
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from  tkinter import *
+import time
+
+#FUNCION PARA ACTUALIZAR LA HORA
+def times():
+	current_time=time.strftime("%H:%M:%S") 
+	clock.config(text=current_time,bg="black",fg="green",font="Arial 50 bold")
+	clock.after(200,times)
+	
+#VENTANA
+root=Tk()
+root.geometry("485x250")
+root.title("Reloj digital con Tkinter")
+clock=Label(root,font=("times",50,"bold"))
+
+clock.grid(row=2,column=1,pady=25,padx=100)
+times()
+
+digi=Label(root,text=" Hora Actual",font="times 24 bold",fg="red")
+digi.grid(row=0,column=1)
+
+root.mainloop()
+
+
+
+
