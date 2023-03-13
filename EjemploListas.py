b248c562a19f29617f8048432d00fac54afdb9b7
Esteban <cheo28sc@hotmail.com>
2023-03-12 18:19:30 -0600

    ejemplos de input en listas y tuplas

--- EjemploListas.py
+++ EjemploListas.py
@@ -1,18 +1,22 @@
 #Problema 1 definir una lista imprima en pantalla
 
-lista = []
+# lista = []
 
-for x in range (5):
-    valor =  int (input("ingrese un valor entero: "))
-    lista.append(valor)
-    #imprimir
-print (lista)
+# for x in range (5):
+#     valor =  int (input("ingrese un valor entero: "))
+#     lista.append(valor)
+#     #imprimir
+# print (lista)
 
 
 #problema 2
 lista2 = []
-valor2 =  int (input("ingrese un valor entero: "))
-while valor != 0:
+
+valor2 =  int(input("ingrese un valor: (0 para finalizar)"))
+
+while valor2 != 0:
+    
     lista2.append(valor2)
-    valor2 =  int (input("ingrese un valor entero: "))
+    valor2 = int(input("ingrese un valor: (0 para finalizar)")) 
 print ("tamaño de la lista ", len(lista2))
+
